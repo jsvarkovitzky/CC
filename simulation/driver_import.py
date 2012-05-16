a3ba22f1c98068753ee2e583db9b15d425498c00
Varkovitzky EC2 <vark@uw.edu>
2012-05-16 18:55:57 +0000

    Adding class to pass in driver parameters

---
+++ simulation/driver_import.py
@@ -0,0 +1,21 @@
+# This program tests the functionality of reading in csv files
+# Jonathan Varkovitzky
+# May 16, 2012
+
+import csv
+from numpy import *
+
+class driver_info:
+    def __init__(self,runid):
+        driver_file = '/home/ubuntu/Driver_Test.csv'
+        driver_block = genfromtxt(driver_file, dtype=None, delimiter=',', names=True)
+        driver_list = driver_block[runid]
+    
+        self.runid = driver_list[0]
+        self.source = driver_list[1]
+        self.refine = driver_list[2]
+
+
+
+
+
