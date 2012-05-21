ff5f7108ae17f2fc6487dd1b8f42703f9e02d97e
Varkovitzky EC2 <vark@uw.edu>
2012-05-21 06:42:43 +0000

    importing user file into get_topo

--- get_topo_s3.py
+++ get_topo_s3.py
@@ -9,7 +9,7 @@ from boto.s3.connection import Location
 import os, sys
 import keys
 
-iiiimport os, sys
+import os, sys
 lib_path = os.path.abspath('../')
 sys.path.append(lib_path)
 import user_info_file
