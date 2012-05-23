76186bb975a896fb20b1c034471463fa26ed2073
Varkovitzky EC2 <vark@uw.edu>
2012-05-23 18:39:15 +0000

    adding driver_importer.py to allow setrun to access it on new machines

--- simulation/driver_import.py
+++ simulation/driver_import.py
@@ -7,13 +7,17 @@ from numpy import *
 
 class driver_info:
     def __init__(self,runid):
-        driver_file = '/home/ubuntu/Driver_Test.csv'
+        driver_file = '/home/ubuntu/Driver.csv'
         driver_block = genfromtxt(driver_file, dtype=None, delimiter=',', names=True)
         driver_list = driver_block[runid-1]
     
         self.runid = driver_list[0]
         self.source = driver_list[1]
-        self.refine = driver_list[2]
+        self.mxnest = driver_list[2]
+        self.inratx = [driver_list[3],driver_list[4],driver_list[5],driver_list[6],driver_list[7]]
+        self.topofile = driver_list[7]
+        self.subdomain = driver_list[8]
+        self.fg_file = driver_list[9]
 
 
 
