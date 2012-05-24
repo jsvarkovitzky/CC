384dac760de460c97c0e3e595e477d3d550bfd00
Varkovitzky EC2 <vark@uw.edu>
2012-05-24 16:33:20 +0000

    reverting to manual topo read as a test

--- simulation/setrun.py
+++ simulation/setrun.py
@@ -303,12 +303,12 @@ def setgeo(rundata):
     topo_list = '/home/ubuntu/topo_list.csv'
     topo_block =  genfromtxt(topo_list, dtype=None, delimiter=',', names=True)
     #Read through row wise in csv file to extract relavent geodata
-    for row in topo_block:
-        fname = topodir + row[0]
-        print "The file being read in is %s." %fname
-        geodata.topofiles.append([row[1], row[2], row[3], row[4], row[5], row[0]])
+    #for row in topo_block:
+    #    fname = topodir + row[0]
+    #    print "The file being read in is %s." %fname
+    #    geodata.topofiles.append([row[1], row[2], row[3], row[4], row[5], row[0]])
     
-    """
+    
     fname = topodir + '/etopo1min139E147E34N41N.asc'
     geodata.topofiles.append([3, 1, 1, 0., 1.e10, fname])
     
@@ -323,7 +323,7 @@ def setgeo(rundata):
      
     fname = topodir + '/cc-1_3sec-c.asc'
     geodata.topofiles.append([-3, 1, 1, 32000, 1.e10, fname])  
-    """
+    
 #    fname = topodir + '/crescent_city_1-3_arc-second_mhw.asc'
 #    geodata.topofiles.append([3, 1, 1, 32000, 1.e10, fname])  
 
