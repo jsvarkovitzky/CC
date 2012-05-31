435a4fb13741774f467502340eda589b91869950
Varkovitzky EC2 <vark@uw.edu>
2012-05-31 18:29:17 +0000

    Testing simplified runSimulation code

--- simulation/runSimulation.py
+++ simulation/runSimulation.py
@@ -6,11 +6,14 @@ from setrun import setrun
 from setplot import setplot                                                     
 from pyclaw.runclaw import runclaw                                              
 from pyclaw.plotters.plotclaw import plotclaw                                   
+import os
                                                                                 
 # initialize rundata using setrun but then change some things for each run:     
 rundata = setrun()
 
-runclaw(xclawcmd = "xgeoclaw", outdir="_output")
-plotclaw(outdir="_output", plotdir="_plots")
+#runclaw(xclawcmd = "xgeoclaw", outdir="_output")
+#plotclaw(outdir="_output", plotdir="_plots")
 
+os.system('make .output')
+os.system('make .plots')
 import upload_results
